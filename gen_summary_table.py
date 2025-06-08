93e3d3ca3630bc6fc8fe17ed5e51b1953d056cfd
nickchengtw <33611456+nickchengtw@users.noreply.github.com>
2025-06-09 03:19:38 +0800

    Feat: add reports with filters

--- gen_summary_table.py
+++ gen_summary_table.py
@@ -11,9 +11,7 @@ def read_csvs_with_folder_name(root_dir, target_filename):
                 df = pd.read_csv(file_path)
                 # Get the folder name (the last part of dirpath)
                 folder_name = os.path.basename(dirpath)
-                # df['source_folder'] = folder_name  # Add as a new column
                 dataframes.append((folder_name, df))
-                # print(f"Loaded from: {folder_name}")
             except Exception as e:
                 print(f"Error reading {file_path}: {e}")
 
@@ -25,7 +23,6 @@ dfs = read_csvs_with_folder_name("reports/", "default.csv")
 for symbol, df in dfs:
     top_mape = round(df.loc[0, 'MAPE'], 2)
     summary_table[symbol] = [top_mape]
-    # print(symbol, top_mape)
 
 for name in ['add_period', 'add_ar', 'tune_close', 'lag_vol_price', 'lag_inst', 'lag_share']:
     table_index.append(name)
@@ -33,17 +30,15 @@ for name in ['add_period', 'add_ar', 'tune_close', 'lag_vol_price', 'lag_inst', 
     for symbol, df in dfs:
         top_mape = round(df.loc[0, 'MAPE'], 2)
         summary_table[symbol].append(top_mape)
-        # print(symbol, top_mape)
 
-table_index.append('with_lag')
+table_index.append('with_future')
 dfs = read_csvs_with_folder_name("reports/", "lag_share.csv")
 for symbol, _ in dfs:
     print(symbol)
     df = pd.read_csv(f"reports/{symbol}/final_{symbol}.csv")
     top_mape = round(df.loc[0, 'MAPE'], 2)
     summary_table[symbol].append(top_mape)
-    # print(symbol, top_mape)
 
 summary_df = pd.DataFrame(summary_table, index=table_index).T
-summary_df['is_lower'] = summary_df['with_lag'] < summary_df['lag_share']
+summary_df['is_lower'] = summary_df['with_future'] < summary_df['lag_share']
 print(summary_df)
